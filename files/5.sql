0aa94b7a5bada80a199233047452b3da22503299
RAKESH <rakesh.s.ee032@gmail.com>
2015-01-12 16:57:38 +0530

    New Features

---
+++ files/5.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `tweets` ADD `favorites` INT NOT NULL AFTER `created_at`;
+ALTER TABLE `tweets` ADD `retweets` INT NOT NULL AFTER `media`;
+
+ALTER TABLE `tweets` ADD INDEX (`favorites`) ;
+ALTER TABLE `tweets` ADD INDEX (`retweets`) ;
